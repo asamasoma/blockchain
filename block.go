f542a040a47ff1248041a530289f8837cf64e2cb
asamasoma <amasa.amos@intentmedia.com>
2018-06-14 13:56:06 -0400

    Persistence and CLI.

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -24,4 +27,31 @@ func NewBlock(data string, PrevBlockHash []byte) *Block {
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Fatal("encode:", err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Fatal("decode:", err)
+	}
+
+	return &block
 }
